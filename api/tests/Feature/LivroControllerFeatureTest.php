5ba0af17a5069ddbe9acbf0f759fa245e47f110c
luzinaldocruz <luzinaldocruz@gmail.com>
2025-03-12 22:44:41 -0300

    refactor: livro test

--- api/tests/Feature/LivroControllerFeatureTest.php
+++ api/tests/Feature/LivroControllerFeatureTest.php
@@ -59,29 +59,4 @@ class LivroControllerFeatureTest extends TestCase
             'AnoPublicacao',
         ]);
     }
-
-    // public function testUpdateLivroNotFound()
-    // {
-    //     // ID de um livro que não existe
-    //     $id = 999;
-
-    //     // Dados atualizados do livro
-    //     $updatedData = [
-    //         'Titulo' => 'Livro Atualizado',
-    //         'Editora' => 'Editora Atualizada',
-    //         'Edicao' => 2,
-    //         'AnoPublicacao' => 2024,
-    //     ];
-
-    //     // Faz uma requisição PUT para a rota de atualização de livro
-    //     $response = $this->putJson("/api/livros/{$id}", $updatedData);
-
-    //     // Verifica o status da resposta
-    //     $response->assertStatus(404);
-
-    //     // Verifica a mensagem de erro
-    //     $response->assertJson([
-    //         'message' => 'Livro not found',
-    //     ]);
-    // }
 }
